b11886a5ccaef0da7fc68bd896b61eed4b9bb982
atp1o2 <andrewpham8@gmail.com>
2016-08-27 14:00:53 -0700

    angular-ui-router setup

--- app/app.js
+++ app/app.js
@@ -2,13 +2,24 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
+  'ui.router',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  // $urlRouterProvider.otherwise("/view1");
+
+  $stateProvider
+    .state('view1', {
+      url: "/view1",
+      templateUrl: 'app/view1/view1.html'
+    })
+
+    .state('view2', {
+      url: "/view2",
+      templateUrl: 'app/view2/view2.html'
+    })
+
 }]);
